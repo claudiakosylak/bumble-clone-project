089ce97b352a8dc366da72d225a617b74e012bb8
Your Name <claudiakosylak@gmail.com>
2023-06-13 15:21:05 -0500

    added routes to sign up and login on splash page

--- react-app/src/components/SplashIndex/index.js
+++ react-app/src/components/SplashIndex/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-
+import "./SplashIndex.css";
+import { NavLink } from "react-router-dom";
 
 function SplashIndex() {
     const user = useSelector(state => state.session.user)
@@ -11,7 +12,18 @@ function SplashIndex() {
     }
 
     return (
-        <div>Hi from the splash page</div>
+        <div className="splash-index-wrapper">
+            <div className="splash-main-image">
+                <div className="splash-main-textarea">
+                    <h2>Stop wasting time with flakes</h2>
+                    <p>Start ACTUALLY meeting with new people in your area! If you already have an account, sign up to use NoFlake on the web. </p>
+                    <div className="splash-login-buttons">
+                        <NavLink to="/signup" className="splash-login-button">Join</NavLink>
+                        <NavLink to="/login" className="splash-login-button">Sign In</NavLink>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
