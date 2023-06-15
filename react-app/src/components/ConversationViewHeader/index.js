999fc0efb18b1778a3473325639377e49eaaab95
Your Name <claudiakosylak@gmail.com>
2023-06-15 11:53:58 -0500

    made match settings dropdown

--- react-app/src/components/ConversationViewHeader/index.js
+++ react-app/src/components/ConversationViewHeader/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./ConversationViewHeader.css";
 import { getDateThunk, getDatesThunk } from "../../store/date";
@@ -9,7 +9,8 @@ function ConversationViewHeader() {
     const allDates = Object.values(allDatesObj)
     const allDateMatchIds = allDates.map(date => date.match_id)
     const currentDate = allDates.find(date => date.match_id === currentMatch.matchId)
-
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
 
     const dispatch = useDispatch();
 
@@ -17,17 +18,46 @@ function ConversationViewHeader() {
         dispatch(getDatesThunk())
     }, [dispatch])
 
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (!ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener("click", closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const ulClassName = "match-settings-dropdown" + (showMenu ? "" : " hidden");
+    const closeMenu = () => setShowMenu(false);
 
     return (
         <div className='conversation-view-header-wrapper'>
             <img src={currentMatch.picture_1}></img>
+            <h2>{currentMatch.first_name}</h2>
             {(allDateMatchIds.includes(currentMatch.matchId)) && (
                 <div className="scheduled-status">
                     <i class="fa-regular fa-calendar-days"></i>
                     <p className="scheduled-status-text">You are scheduled for a date on {currentDate.scheduled_date}</p>
                 </div>
             )}
-            <h2>{currentMatch.first_name}</h2>
+            <i class="fa-solid fa-ellipsis-vertical" onClick={openMenu}></i>
+            <ul className={ulClassName} ref={ulRef}>
+                <div>
+
+                    <li>Schedule a date</li>
+                    <li>Report user</li>
+                </div>
+            </ul>
         </div>
     )
 }
--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignupFormModal";
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -34,7 +31,7 @@ function ProfileButton({ user }) {
     dispatch(logout());
   };
 
-  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+  const ulClassName = "match-settings-dropdown" + (showMenu ? "" : " hidden");
   const closeMenu = () => setShowMenu(false);
 
   return (
@@ -43,27 +40,19 @@ function ProfileButton({ user }) {
         <img src={user.picture_1} className="current-user-image" />
       </div>
       <ul className={ulClassName} ref={ulRef}>
-        {user ? (
+        {user && (
           <>
+          <div>
+          <i class="fa-solid fa-angle-left" onClick={closeMenu}></i>
+          </div>
+          <div>
+
             <li>{user.username}</li>
             <li>{user.email}</li>
             <li>
               <button onClick={handleLogout}>Log Out</button>
             </li>
-          </>
-        ) : (
-          <>
-            <OpenModalButton
-              buttonText="Log In"
-              onItemClick={closeMenu}
-              modalComponent={<LoginFormModal />}
-            />
-
-            <OpenModalButton
-              buttonText="Sign Up"
-              onItemClick={closeMenu}
-              modalComponent={<SignupFormModal />}
-            />
+          </div>
           </>
         )}
       </ul>
