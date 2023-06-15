a79ae567899162ab7869aaef3a9eb8d32520cc30
Your Name <claudiakosylak@gmail.com>
2023-06-15 11:32:22 -0500

    scheduled date shows up on message page

--- react-app/src/components/ConversationViewHeader/index.js
+++ react-app/src/components/ConversationViewHeader/index.js
@@ -7,10 +7,9 @@ function ConversationViewHeader() {
     const currentMatch = useSelector(state => state.match.currentMatch)
     const allDatesObj = useSelector(state => state.date.allDates)
     const allDates = Object.values(allDatesObj)
-    console.log("ALL DATES: ", allDates)
     const allDateMatchIds = allDates.map(date => date.match_id)
+    const currentDate = allDates.find(date => date.match_id === currentMatch.matchId)
 
-    console.log("ALL DATE MATCH IDS", allDateMatchIds)
 
     const dispatch = useDispatch();
 
@@ -23,7 +22,10 @@ function ConversationViewHeader() {
         <div className='conversation-view-header-wrapper'>
             <img src={currentMatch.picture_1}></img>
             {(allDateMatchIds.includes(currentMatch.matchId)) && (
-                <p>Scheduled</p>
+                <div className="scheduled-status">
+                    <i class="fa-regular fa-calendar-days"></i>
+                    <p className="scheduled-status-text">You are scheduled for a date on {currentDate.scheduled_date}</p>
+                </div>
             )}
             <h2>{currentMatch.first_name}</h2>
         </div>
