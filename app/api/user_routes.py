84fd3e468d5a5f3b3031b0e6b8bcfcff2aa819ea
Your Name <claudiakosylak@gmail.com>
2023-06-16 13:15:37 -0500

    added wtform for update about

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.models import User
 
 user_routes = Blueprint('users', __name__)
@@ -13,6 +13,14 @@ def users():
     """
     users = User.query.all()
     return {'users': [user.to_dict() for user in users]}
+
+
+@user_routes.route('/about', methods=["PUT"])
+@login_required
+def update_about():
+    """ Updates the current user's about information """
+    user = User.query.get(current_user.id)
+    user.about = 
 
 
 @user_routes.route('/<int:id>')
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .date_request_form import RequestDateForm
 from .date_report_form import DateReportForm
+from .update_about_form import UpdateAboutForm
