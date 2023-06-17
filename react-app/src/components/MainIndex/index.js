c79199971475784c8991fc696712fb2b677c3401
Your Name <claudiakosylak@gmail.com>
2023-06-16 20:26:52 -0500

    clear current match on return to main index

--- react-app/src/components/MainIndex/index.js
+++ react-app/src/components/MainIndex/index.js
@@ -1,14 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import LeftMatchesBar from "../LeftMatchesBar";
 import "./MainIndex.css";
 import PotentialMatchBrowse from "../PotentialMatchBrowse";
+import { clearCurrentMatch } from "../../store/match";
 
 
 function MainIndex({isLoaded}) {
     const user = useSelector(state => state.session.user)
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(clearCurrentMatch())
+    }, [dispatch])
     if (!user) {
         return <Redirect to="/" />
     }
