61963ac831abf871d1bffcb1889d47ec24abd6d1
Claudia Kosylak <claudiakosylak@gmail.com>
2024-02-04 10:58:31 +0100

    refactored sass for login page

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { login } from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
-import './LoginForm.css';
-import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import { Redirect, NavLink } from "react-router-dom";
+import styles from "./LoginFormPage.module.sass";
+import globalStyles from "../../global.module.sass";
 
 function LoginFormPage() {
   const dispatch = useDispatch();
@@ -13,24 +13,10 @@ function LoginFormPage() {
   const [errors, setErrors] = useState([]);
 
 
-  useEffect(() => {
-    const newErrors = {}
-    if (password.length > 20) newErrors.password = "Password must be less than 20 characters"
-    if (email.length > 50) newErrors.password = "Email must be less than 50 characters long"
-    setErrors(newErrors)
-  }, [password, email])
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = await dispatch(login(email, password));
     if (data) {
-      // const newErrors = {}
-      // if (data.email[0]) {
-      //   newErrors.email = data.email[0]
-      // }
-      // if (data.password[0]) {
-      //   newErrors.password = data.password[0]
-      // }
       setErrors(data);
     }
   };
@@ -38,49 +24,48 @@ function LoginFormPage() {
   if (sessionUser) return <Redirect to="/app" />;
 
   const demoLogin = async () => {
-    await dispatch(login("demo@aa.io", "password"))
-  }
+    await dispatch(login("demo@aa.io", "password"));
+  };
 
   return (
-    <div className="login-form-wrapper">
-      <NavLink className="signup-logo-wrapper" to="/">
-        <i class="fa-regular fa-snowflake"></i>
+    <div className={globalStyles.login_wrapper}>
+      <NavLink className={globalStyles.login_logo} to="/">
+        <i className="fa-regular fa-snowflake"></i>
         <h1>noFlake</h1>
       </NavLink>
       <p>Welcome back! Please enter your credentials below.</p>
-      <form onSubmit={handleSubmit} className="login-form">
-
-        {/* Email */}
+      <form onSubmit={handleSubmit} className={styles.form}>
         <input
           type="text"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Email address"
           required
+          maxLength={50}
         />
-
-        {/* {errors.email && (
-          <p>{errors.email}</p>
-        )} */}
-
-
-        {/* Password */}
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
           required
+          maxLength={20}
         />
         {Object.values(errors).length > 0 && (
           <p className="errors">Invalid credentials</p>
         )}
-        {/* {errors.password && (
-          <p>{errors.password}</p>
-        )} */}
-        <button type="submit" className="login-submit">Log In</button>
-        <button onClick={demoLogin} className="demo-login">Demo User Login</button>
-        <p>Don't have an account? <NavLink to="/signup" className="register-here">Register here</NavLink></p>
+        <button type="submit" className={`${globalStyles.login_button} ${globalStyles.login_submit}`}>
+          Log In
+        </button>
+        <button onClick={demoLogin} className={`${globalStyles.login_button} ${styles.demo}`}>
+          Demo User Login
+        </button>
+        <div className={globalStyles.login_switch}>
+          Don't have an account?{" "}
+          <NavLink to="/signup" className={globalStyles.login_switch_link}>
+            Register here
+          </NavLink>
+        </div>
       </form>
     </div>
   );
