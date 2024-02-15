a29c790b94f3ffa9abb558979a0d7c65cd895f46
Claudia Kosylak <claudiakosylak@gmail.com>
2024-02-15 18:35:07 +0100

    refactoring css conversation view index

--- react-app/src/components/ConversationViewIndex/index.js
+++ react-app/src/components/ConversationViewIndex/index.js
@@ -1,47 +1,43 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import "./ConversationViewIndex.css";
 import LeftMatchesBar from "../LeftMatchesBar";
 import RightProfileBar from "../RightProfileBar";
 import MatchMessages from "../MatchMessages";
 import { Redirect } from "react-router-dom";
 import ConversationViewHeader from "../ConversationViewHeader";
 import { getDateRequestsThunk } from "../../store/date";
+import styles from "./ConversationViewIndex.module.sass";
 
 function ConversationViewIndex({ isLoaded }) {
-    const user = useSelector(state => state.session.user)
-    const currentMatch = useSelector(state => state.match.currentMatch)
-    const dateRequestsObj = useSelector(state => state.date.dateRequests)
-    const dateRequests = Object.values(dateRequestsObj)
-    const dispatch = useDispatch()
+  const user = useSelector((state) => state.session.user);
+  const currentMatch = useSelector((state) => state.match.currentMatch);
+  const dateRequestsObj = useSelector((state) => state.date.dateRequests);
+  const dateRequests = Object.values(dateRequestsObj);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getDateRequestsThunk())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(getDateRequestsThunk());
+  }, [dispatch]);
 
-    if (!user) {
-        return <Redirect to="/" />
-    }
+  if (!user) {
+    return <Redirect to="/" />;
+  }
 
-    if(!currentMatch.id) {
-        return <Redirect to="/app" />
-    }
-    return (
-        <div className="conversation-view-index-wrapper">
-            <LeftMatchesBar isLoaded={isLoaded} />
-            <div className="conversation-view-right-side">
-                <div className="messages-container">
-
-                    <ConversationViewHeader dateRequests={dateRequests}/>
-                    <MatchMessages />
-                </div>
-
-
-                <RightProfileBar />
-            </div>
+  if (!currentMatch.id) {
+    return <Redirect to="/app" />;
+  }
+  return (
+    <div className={styles.wrapper}>
+      <LeftMatchesBar isLoaded={isLoaded} />
+      <div className={styles.main_content}>
+        <div className={styles.main_content_inner}>
+          <ConversationViewHeader dateRequests={dateRequests} />
+          <MatchMessages />
         </div>
-    )
+        <RightProfileBar />
+      </div>
+    </div>
+  );
 }
-
 
 export default ConversationViewIndex;
